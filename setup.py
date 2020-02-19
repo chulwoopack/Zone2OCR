02c4e12108686da1c16413af98f57fdf64efaa5b
chulwoo.pack <chulwoo.pack@huskers.unl.edu>
2020-02-18 21:17:37 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='zone2ocr',
+      version='0.1.0',
+      license='GPL',
+      url='https://github.com/chulwoopack/Zone2OCR',
+      description='Segmentation mapping tool for document processing',
+      packages=find_packages(exclude=['exps*']),
+      install_requires=[
+        'numpy==1.16.2',
+        'opencv-python',
+        'shapely',
+        'tqdm==4.31.1',
+      ],
+      zip_safe=False)
