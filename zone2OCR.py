0ddd41cba71faab253ec8b6270d855a31cec6348
chulwoo.pack <chulwoo.pack@huskers.unl.edu>
2020-02-25 17:09:13 -0600

    Update zone2OCR.py

--- zone2OCR.py
+++ zone2OCR.py
@@ -111,72 +111,103 @@ for idx in tqdm(len(zone_xml_file_paths)):
 
 
     """
-    Mapping
+    MAPPING
     """
     # output json
     map_json = []
 
-    for zone_idx,zone_textBlock in enumerate(zone_textBlocks):
+    for zone_idx,zone_textBlock in enumerate(tqdm(zone_textBlocks)):
         # zone coordinates
-        zone_width  = int(zone_textBlock.attributes["WIDTH"].value)
-        zone_height = int(zone_textBlock.attributes["HEIGHT"].value)
-        zone_vpos   = int(zone_textBlock.attributes["VPOS"].value)
-        zone_hpos   = int(zone_textBlock.attributes["HPOS"].value)
-        
+        zone_width  = int(float(zone_textBlock.attributes["WIDTH"].value))
+        zone_height = int(float(zone_textBlock.attributes["HEIGHT"].value))
+        zone_vpos   = int(float(zone_textBlock.attributes["VPOS"].value))
+        zone_hpos   = int(float(zone_textBlock.attributes["HPOS"].value))
+
         zone_p1 = (zone_hpos,zone_vpos)
         zone_p2 = ((zone_hpos+zone_width),zone_vpos)
         zone_p3 = (zone_hpos,(zone_vpos+zone_height))
         zone_p4 = ((zone_hpos+zone_width),(zone_vpos+zone_height))
-        
+
         zone_coord = [zone_p3, zone_p4, zone_p2, zone_p1]
-        
+
         # Build json
         _textBlock_xml = {}
         _textBlock_xml["zone_coord"] = [list(zone_p3), list(zone_p4), list(zone_p2), list(zone_p1)]
-        _matching_ocr_textBlocks = []
-        _matching_ocr_contents   = []
-        
+        _set_ocr_textBlocks = []
+        _set_ocr_contents   = []
+
+        _sub_ocr_contents   = []
+
         for ocr_idx,ocr_textBlock in enumerate(ocr_textBlocks):
             # OCR coordinates
-            ocr_width  = int(ocr_textBlock.attributes["WIDTH"].value)
-            ocr_height = int(ocr_textBlock.attributes["HEIGHT"].value)
-            ocr_vpos   = int(ocr_textBlock.attributes["VPOS"].value)
-            ocr_hpos   = int(ocr_textBlock.attributes["HPOS"].value)
-        
+            ocr_width  = int(float(ocr_textBlock.attributes["WIDTH"].value))
+            ocr_height = int(float(ocr_textBlock.attributes["HEIGHT"].value))
+            ocr_vpos   = int(float(ocr_textBlock.attributes["VPOS"].value))
+            ocr_hpos   = int(float(ocr_textBlock.attributes["HPOS"].value))
+
             width  = int(ocr_width*factor)
             height = int(ocr_height*factor)
             vpos   = int(ocr_vpos*factor)
             hpos   = int(ocr_hpos*factor)
-            
+
             ocr_p1 = (hpos,vpos)
             ocr_p2 = ((hpos+width),vpos)
             ocr_p3 = (hpos,(vpos+height))
             ocr_p4 = ((hpos+width),(vpos+height))
-            
+
             ocr_coord = [ocr_p3, ocr_p4, ocr_p2, ocr_p1]
-            
+
             # Find matching regions
             zone_polygon = Polygon(zone_coord)
             ocr_polygon  = Polygon(ocr_coord)
-            
+
             iou = zone_polygon.intersection(ocr_polygon).area / zone_polygon.union(ocr_polygon).area
-            
+
             if(iou >= IOU_THRESHOLD):
-                # OCR Contents
-                contents = ''
+                # Set of OCR touching the Zone
+                set_contents = ''
+                sub_contents = ''
                 ocr_textLines = ocr_textBlock.getElementsByTagName('TextLine')
+
                 for ocr_textline in ocr_textLines:
+                    # Textline coordinates
+                    txt_width  = int(float(ocr_textline.attributes["WIDTH"].value))
+                    txt_height = int(float(ocr_textline.attributes["HEIGHT"].value))
+                    txt_vpos   = int(float(ocr_textline.attributes["VPOS"].value))
+                    txt_hpos   = int(float(ocr_textline.attributes["HPOS"].value))
+
+                    width  = int(txt_width*factor)
+                    height = int(txt_height*factor)
+                    vpos   = int(txt_vpos*factor)
+                    hpos   = int(txt_hpos*factor)
+
+                    txt_p1 = (hpos,vpos)
+                    txt_p2 = ((hpos+width),vpos)
+                    txt_p3 = (hpos,(vpos+height))
+                    txt_p4 = ((hpos+width),(vpos+height))
+
+                    txt_coord  = [txt_p3, txt_p4, txt_p2, txt_p1]
+
+                    txt_polygon = Polygon(txt_coord)
+
+                    # Textline string
                     strings = ocr_textline.getElementsByTagName('String')
                     for string in strings:
-                        contents += (str(string.attributes["CONTENT"].value) + ' ')
+                        set_contents += (str(string.attributes["CONTENT"].value) + ' ')
+
+                        # Subset of OCR within the Zone
+                        if(zone_polygon.intersects(txt_polygon)):
+                            sub_contents += (str(string.attributes["CONTENT"].value) + ' ')
 
                 # Build json
-                _matching_ocr_textBlocks.append([list(ocr_p3), list(ocr_p4), list(ocr_p2), list(ocr_p1)])
-                _matching_ocr_contents.append(contents)
+                _sub_ocr_contents.append(sub_contents)
+                _set_ocr_textBlocks.append([list(ocr_p3), list(ocr_p4), list(ocr_p2), list(ocr_p1)])
+                _set_ocr_contents.append(set_contents)
 
         # Build json
-        _textBlock_xml["ocr_coords"] = _matching_ocr_textBlocks
-        _textBlock_xml["ocr_texts"] =_matching_ocr_contents
+        _textBlock_xml["zone_texts"] = _sub_ocr_contents
+        _textBlock_xml["ocr_coords"] = _set_ocr_textBlocks
+        _textBlock_xml["ocr_texts"]  = _set_ocr_contents
         map_json.append(_textBlock_xml)
 
     # Save json
@@ -184,5 +215,6 @@ for idx in tqdm(len(zone_xml_file_paths)):
     out_json_filename = os.path.basename(ocr_xml_file_path).split('.')[0] + '.json'
     with open(os.path.join(SAVE_PATH,out_json_filename),'w') as out_json_fp:
         json.dump(map_json, out_json_fp) 
+
 
 print("Done.")
